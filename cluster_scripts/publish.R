f3e61f800c6d81f1d8b4e07cd8dfcd912e121459
schae211 <schae211@msu.edu>
2021-12-23 16:28:58 +0100

    changing wflow_publish options

--- cluster_scripts/publish.R
+++ cluster_scripts/publish.R
@@ -4,5 +4,5 @@ message(paste0("Publishing: ", cmd.arg1))
 
 library(workflowr)
 
-wflow_publish(paste0("analysis/", cmd.arg1))
+wflow_publish(paste0("analysis/", cmd.arg1), delete_cache=TRUE, verbose=TRUE)
 
