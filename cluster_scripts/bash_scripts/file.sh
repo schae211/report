5fb0650eee6b68284beb63f96419d05c88c468fd
schae211 <schae211@msu.edu>
2021-12-23 15:59:24 +0100

    added wd to file.sh

--- cluster_scripts/bash_scripts/file.sh
+++ cluster_scripts/bash_scripts/file.sh
@@ -10,6 +10,9 @@ source /home/bq_pschaefer/.bashrc
 # Actiavte my conda environment
 conda activate misty_benchmark
 
+# Set working directory to the report
+cd /net/data.isilon/ag-saez/bq_pschaefer/report
+
 # Run Rscript
 echo "Rscript /net/data.isilon/ag-saez/bq_pschaefer/report/cluster_scripts/$FILE $VIEW $ALG"
 Rscript /net/data.isilon/ag-saez/bq_pschaefer/report/cluster_scripts/$FILE $VIEW $ALG
