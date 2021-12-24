dcf4c700c873ffd991ac673af9067a756b40f43a
schae211 <schae211@msu.edu>
2021-12-24 07:47:55 +0100

    refactored hyperparamter optim scripts

--- cluster_scripts/hyperparameter/lboost_mibi_tnbc.R
+++ cluster_scripts/hyperparameter/lboost_mibi_tnbc.R
@@ -1,35 +1,52 @@
 
 ##### Command line args ##### 
-cmd.arg = as.numeric(commandArgs(trailingOnly = TRUE))[1]
+cmd.arg1 = as.numeric(commandArgs(trailingOnly = TRUE))[1]
+
+cmd.arg2 = as.numeric(commandArgs(trailingOnly = TRUE))[2]
 
 #####  Packages ##### 
 library(mistyR)
 library(future)
-plan("multisession", workers=16)
+plan("multisession")
 library(tidyverse)
 
 ##### Paths ##### 
-input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/mibi_tnbc/"
-output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/mibi_tnbc/LBOOST_hyper/"
+if (cmd.arg1 == 1) {
+  input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/synthetic/"
+  input <- "l12.RDS"
+  output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/synthetic/LBOOST_hyper/"
+} else if (cmd.arg1 == 2) {
+  input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/mibi_tnbc/"
+  input <- "standard_views.RDS"
+  output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/mibi_tnbc/LBOOST_hyper/"
+} else if (cmd.arg1 == 3) {
+  input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/merfish_bc/"
+  input <- "hvg_views.RDS"
+  output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/merfish_bc/LBOOST_hyper"
+}
 ifelse(!dir.exists(output.path), dir.create(output.path), FALSE)
 
 #####  Input ##### 
-misty.views.smp <- readRDS(paste0(input.path, "standard_views.RDS"))
+misty.views.smp <- readRDS(paste0(input.path, input))
 
-##### Parameters ###### -> 7
+##### Parameters ###### -> 8
+
+# Maximum Boosting Rounds
+nroundss <- c(2, 4, 8, 16, 32, 10, 10, 10)
 
 # L2 regularization term on weights: Default: 0
-lambdas <- c(0, 0.5, 1, 5, 0, 0, 0)
+lambdas <- c(0, 0, 0, 0, 0, 0.5, 0, 0)
 
 # L1 regularization term on weights: Default: 0
-alphas <- c(0, 0, 0, 0, 0.5, 1, 5)
+alphas <- c(0, 0, 0, 0, 0, 0 , 1, 5)
 
-map(seq_len(length(lambdas)), ~ paste(lambdas[.x], alphas[.x],
+map(seq_len(length(lambdas)), ~ paste(nroundss[.x], lambdas[.x], alphas[.x],
                                    sep = "_"))
 
 
 ##### Run MISTy #####
-out <- paste("LBOOST_hyper", lambdas[cmd.arg], alphas[cmd.arg], sep = "_")
+out <- paste("LBOOST_hyper", nroundss[cmd.arg2], lambdas[cmd.arg2], 
+             alphas[cmd.arg2], sep = "_")
 
 purrr::iwalk(misty.views.smp, function(smp.views, smp.name) {
   run_misty(views = smp.views,
@@ -38,6 +55,7 @@ purrr::iwalk(misty.views.smp, function(smp.views, smp.name) {
             seed = 42,
             cv.folds = 10,
             booster = "gblinear",
-            lambda = lambdas[cmd.arg],
-            alpha = alphas[cmd.arg])
+            nrounds = nroundss[cmd.arg2],
+            lambda = lambdas[cmd.arg2],
+            alpha = alphas[cmd.arg2])
 })
--- cluster_scripts/hyperparameter/mars_hyper.R
+++ cluster_scripts/hyperparameter/mars_hyper.R
@@ -1,21 +1,33 @@
 
 ##### Command line args ##### 
-cmd.arg = as.numeric(commandArgs(trailingOnly = TRUE))[1]
+cmd.arg1 = as.numeric(commandArgs(trailingOnly = TRUE))[1]
+
+cmd.arg2 = as.numeric(commandArgs(trailingOnly = TRUE))[2]
 
 #####  Packages ##### 
 library(mistyR)
 library(future)
-plan("multisession", workers=24)
+plan("multisession")
 library(tidyverse)
-library(tiff)
 
 ##### Paths ##### 
-input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/mibi_tnbc/"
-output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/mibi_tnbc/MARS_hyper/"
+if (cmd.arg1 == 1) {
+  input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/synthetic/"
+  input <- "l12.RDS"
+  output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/synthetic/MARS_hyper/"
+} else if (cmd.arg1 == 2) {
+  input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/mibi_tnbc/"
+  input <- "standard_views.RDS"
+  output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/mibi_tnbc/MARS_hyper/"
+} else if (cmd.arg1 == 3) {
+  input.path <- "/net/data.isilon/ag-saez/bq_pschaefer/MISTY_VIEWS/merfish_bc/"
+  input <- "hvg_views.RDS"
+  output.path <- "/net/data.isilon/ag-saez/bq_pschaefer/OUTPUT/merfish_bc/MARS_hyper"
+}
 ifelse(!dir.exists(output.path), dir.create(output.path), FALSE)
 
 #####  Input ##### 
-misty.views.smp <- readRDS(paste0(input.path, "standard_views.RDS"))
+misty.views.smp <- readRDS(paste0(input.path, input))
 
 ##### Parameters ######
 
@@ -39,16 +51,16 @@ map(seq_len(length(penalties)), ~ paste(penalties[.x], degrees[.x],
 
 
 ##### Run MISTy #####
-out <- paste("MARS_hyper", penalties[cmd.arg], degrees[cmd.arg], 
-             fastks[cmd.arg], pmethods[cmd.arg], sep = "_")
+out <- paste("MARS_hyper", penalties[cmd.arg2], degrees[cmd.arg2], 
+             fastks[cmd.arg2], pmethods[cmd.arg2], sep = "_")
 purrr::iwalk(misty.views.smp, function(smp.views, smp.name) {
   run_misty(views = smp.views,
             results.folder = paste0(output.path, out, "/", smp.name),
             model.function = mars_model,
             seed = 42,
             cv.folds = 10,
-            degree = degrees[cmd.arg],
-            penalty = penalties[cmd.arg],
-            fast.k = fastks[cmd.arg],
-            pmethod = pmethods[cmd.arg])
+            degree = degrees[cmd.arg2],
+            penalty = penalties[cmd.arg2],
+            fast.k = fastks[cmd.arg2],
+            pmethod = pmethods[cmd.arg2])
 })
